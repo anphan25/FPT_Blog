3abbc42f99026c237a0d1793c1f9ca8885f02c80
chilliambush <letranmydan@outlook.com-m>
2021-09-22 22:56:49 +0700

    login nè hihi

--- src/java/swp/controller/LoginServlet.java
+++ src/java/swp/controller/LoginServlet.java
@@ -8,16 +8,26 @@ package swp.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import swp.account.AccountDAO;
+import swp.account.AccountDTO;
 
 /**
  *
  * @author ASUS
  */
+
 public class LoginServlet extends HttpServlet {
+    private final String HOME_PAGE = "homePage";
+    private final String LOGIN_INVALID_PAGE = "loginPage";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,17 +41,26 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        String url = LOGIN_INVALID_PAGE;
+        try {
+            String email = request.getParameter("txtEmail");
+            String password = request.getParameter("txtPassword");
+            AccountDAO dao = new AccountDAO();
+            AccountDTO result = dao.getUser(email, password);
+            // boolean result = dao.test(email, password);
+            if(result!=null){
+                url = HOME_PAGE;
+                HttpSession session = request.getSession();
+                session.setAttribute("LOGIN", "true");
+            }
+        } catch (SQLException sq) {
+            log("LoginServlet_SQLException "+sq.getMessage());
+        }catch(NamingException ne){
+            log("LoginServlet_NamingException "+ne.getMessage());
+        }finally{
+            response.sendRedirect(url);
+            out.close();
         }
     }
 
