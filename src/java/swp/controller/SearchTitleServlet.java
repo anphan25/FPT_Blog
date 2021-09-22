45ab39a90e0687d571be35c7177efe6b6ac87086
phuxiteo3 <vanphuong0606@gmail.com>
2021-09-22 20:48:42 +0700

    getPostsByTitle

---
+++ src/java/swp/controller/SearchTitleServlet.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swp.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import swp.post.PostDAO;
+import swp.post.PostDTO;
+
+/**
+ *
+ * @author Dell
+ */
+@WebServlet(name = "SearchTitleServlet", urlPatterns = {"/SearchTitleServlet"})
+public class SearchTitleServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        ServletContext context = request.getServletContext();
+        Map<String, String> roadmap = (Map<String, String>) context.getAttribute("ROADMAP");
+        String url = roadmap.get("searchResultPage");
+
+        try {
+            String title = request.getParameter("titleValue");
+            title = title.trim();
+            ArrayList<PostDTO> list = PostDAO.getPostsByTitle(title);
+            request.setAttribute("SEARCHLIST_TITLE ", list);
+            
+        } catch (Exception e) {
+            log("Errot at Search Title Servlet: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
