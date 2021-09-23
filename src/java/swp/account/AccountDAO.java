e08e09d9859cb990b4692e124a6a315bb5b284f8
anphan25 <73126703+anphan25@users.noreply.github.com>
2021-09-23 16:51:03 +0700

    thêm lại cái account

--- src/java/swp/account/AccountDAO.java
+++ src/java/swp/account/AccountDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package swp.account;
 
 import java.sql.Connection;
@@ -109,3 +104,4 @@ public class AccountDAO {
         return null;
     }
 }
+
