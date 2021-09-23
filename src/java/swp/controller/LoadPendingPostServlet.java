ef3456642324621d82fd96ef8d61ecccea4431f5
Saphareong <tranhainam12072001@gmail.com>
2021-09-23 09:41:21 +0700

    nho sua lai ten package va bien rm etc

---
+++ src/java/swp/controller/LoadPendingPostServlet.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package namth.PendingPost;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import namth.Authenticate.AccountDTO;
+import our.Controller.RoadMap;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name = "LoadPendingPostServlet", urlPatterns =
+{
+    "/LoadPendingPostServlet"
+})
+public class LoadPendingPostServlet extends HttpServlet
+{
+    private final String HOME_PAGE = "homePage";
+    private final String PENDING_PAGE = "pendingPostListPage";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NamingException, SQLException
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        ServletContext context = request.getServletContext();
+        RoadMap rm = (RoadMap) context.getAttribute("ROAD_MAP");
+        String url = rm.getResource(HOME_PAGE);
+        
+        try
+        {
+            HttpSession session = request.getSession(false);
+            if(session != null)
+            {
+                //chỗ này sẽ cần phải thay đổi cách thức hđ DAO
+                if(session.getAttribute("ACCOUNT") != null)
+                {
+                    AccountDTO accInfo = (AccountDTO) session.getAttribute("ACCOUNT");
+                    String role = accInfo.getRoleID();
+                    if(role.equals("M"))
+                    {
+                        PendingPostDAO dao = new PendingPostDAO();
+                        ArrayList<PendingPostDTO> dto = dao.getAllWaitingPost();
+                        if(!dto.isEmpty())
+                        {
+                            request.setAttribute("PENDING_LIST", dto);
+                            url = rm.getResource(PENDING_PAGE);
+                        }//kết thúc nếu dto có dữ liệu
+                        //có thể thêm else để bổ sung popup thông báo ko có pending post.
+                    }//kết thúc check mentor
+                }//kết thúc việc check đăng nhập
+            }//kết thúc check session
+        }
+        catch(NamingException ex)
+        {
+            log(" Naming: " + ex.getMessage());
+        }
+        catch(SQLException ex)
+        {
+            log(" SQL: " + ex.getMessage());
+        }
+        finally
+        {
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        try
+        {
+            processRequest(request, response);
+        } catch (NamingException ex)
+        {
+            Logger.getLogger(LoadPendingPostServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(LoadPendingPostServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        try
+        {
+            processRequest(request, response);
+        } catch (NamingException ex)
+        {
+            Logger.getLogger(LoadPendingPostServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(LoadPendingPostServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo()
+    {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/swp/pendingpost/PendingPostDAO.java
@@ -0,0 +1,70 @@
+
+package namth.PendingPost;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.naming.NamingException;
+import our.Database.DButils;
+
+
+public class PendingPostDAO implements Serializable
+{
+    public ArrayList<PendingPostDTO> getAllWaitingPost() throws NamingException, SQLException
+    {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        ArrayList<PendingPostDTO> dto = new ArrayList<>();
+        try 
+        {
+            conn = DButils.getConnection();
+            if (conn != null) 
+            {
+                String sql = "SELECT tag, title, postid, emailpost "
+                        + ", Day(p.createdDate) as createdDay, month(p.createdDate) as createdMonth, year(p.createdDate) as createdYear"
+                        + ", a.name, a.image "
+                        + "FROM tblPosts p, tblAccounts a "
+                        + "WHERE p.emailpost = a.email and p.StatusPost = ? "
+                        + "ORDER BY p.createdDate asc";
+                //sau lày nếu hệ thống chạy chậm hơn con rùa trước nhà t thì hãy chuyển sàng dùng join và on
+                //vòng lặp while ở dưới sẽ có chữ continue
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, "WFA");
+                rs = stm.executeQuery();
+                while (rs.next()) 
+                {
+                    String tag = rs.getString("tag");
+                    String title = rs.getString("title");
+                    String createDate = rs.getString("createdDay") + "/" + rs.getString("createdMonth") + "/" + rs.getString("createdYear");
+                    String name = rs.getString("name");
+                    String url = rs.getString("image");
+                    PendingPostDTO dummy = new PendingPostDTO(title, tag, url, createDate, name);
+                    boolean checker = dto.add(dummy);
+                    if(!checker)
+                    {
+                        throw new SQLException("oi dit me cuoc doi");
+                    }
+                }
+                return dto;
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        }
+        return null;
+    }
+    
+    //private boolean checkResult
+}
