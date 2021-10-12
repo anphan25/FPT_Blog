a5125228621ebfc7c0ceef3a3bbbbb95dedf82f4
phuxiteo3 <vanphuong0606@gmail.com>
2021-10-12 21:39:22 +0700

    Sửa Award trong ProfileDTO > chỉnh các hàm lquan

--- src/java/swp/profile/ProfileDAO.java
+++ src/java/swp/profile/ProfileDAO.java
@@ -19,7 +19,6 @@ public class ProfileDAO implements Serializable {
         PreparedStatement stm = null;
         ResultSet rs = null;
         try {
-            //cố gắng thông ass DB
             con = DBHelper.makeConnection();
             if (con != null) {
                 String sql = "SELECT a.Name, a.Gender, a.Campus, r.RoleName, a.Image "
@@ -28,21 +27,14 @@ public class ProfileDAO implements Serializable {
                 stm = con.prepareStatement(sql);
                 stm.setString(1, email);
                 rs = stm.executeQuery();
-                //trỏ tới dòng sql đầu tiên
                 if (rs.next()) {
-                    String name = rs.getString(1);
-                    boolean gioitinh = rs.getBoolean(2);
-                    String gender;
-                    if (gioitinh) {
-                        gender = "Male";
-                    } else {
-                        gender = "Female";
-                    }
-                    String campus = rs.getString(3);
-                    String RoleID = rs.getString(4);
-                    String URL = rs.getString(5);
-                    int Awards = totalReward(email);
-                    ProfileDTO dto = new ProfileDTO(name, campus, email, RoleID, URL, Awards, gender);
+                    String name = rs.getNString("Name");
+                    String gender = rs.getBoolean("Gender") ? "Male" : "Female";
+                    String campus = rs.getNString("Campus");
+                    String roleID = rs.getString("RoleName");
+                    String imageURL = rs.getString("Image");
+                    ArrayList<Integer> awards = totalReward(email);
+                    ProfileDTO dto = new ProfileDTO(name, campus, email, roleID, imageURL, gender, awards);
                     return dto;
                 }
             }
@@ -60,30 +52,22 @@ public class ProfileDAO implements Serializable {
         return null;
     }
 
-    private int totalReward(String email) throws NamingException, SQLException {
+    private ArrayList<Integer> totalReward(String email) throws NamingException, SQLException {
         Connection con = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
+        ArrayList<Integer> list = new ArrayList<>();
         try {
-            //cố gắng thông ass DB
             con = DBHelper.makeConnection();
             if (con != null) {
-//                String sql = "SELECT COUNT(p.PostID) AS TOTAL "
-//                        +   "FROM tblAccounts a LEFT JOIN tblposts p "
-//                        +   "ON p.AwardID is not null AND a.Email = p.EmailPost "
-//                        +   "GROUP BY a.Email HAVING a.Email = ?";
-                String sql = "SELECT COUNT(AwardDetailID) AS TOTAL "
+                String sql = "SELECT awardid "
                         + "FROM tblAwardDetails "
                         + "WHERE EmailStudent = ?";
                 stm = con.prepareStatement(sql);
                 stm.setString(1, email);
                 rs = stm.executeQuery();
-                //trỏ tới dòng sql đầu tiên
-                if (rs.next()) {
-                    return rs.getInt("TOTAL");
-                } else {
-                    //dòng này chỉ throw KHI email truyền vào ko có ở database (email để xem profile)
-                    throw new SQLException("oi dit me co hacker a ??");
+                while (rs.next()) {
+                    list.add(rs.getInt("AwardID"));
                 }
             }
         } finally {
@@ -97,7 +81,8 @@ public class ProfileDAO implements Serializable {
                 stm.close();
             }
         }
-        return 0; //nếu query ở trên có thể bị lỗi thì ít ra vẫn chạy nuột (phần này sẽ test sau)
+        return list; //nếu query ở trên có thể bị lỗi thì ít ra vẫn chạy nuột (phần này sẽ test sau)
+        //update db > Chỗ này awards là list nó có thể nhận được mảng null, 1, 2, 3 phần tử
     }
 
     private ArrayList<PostDTO> list = new ArrayList<>();
@@ -125,9 +110,9 @@ public class ProfileDAO implements Serializable {
                 //GaRBagE Cô lét sần
                 PostDAO dao = new PostDAO(); //gọi DAO trong DAO. DAO-ception
                 while (rs.next()) {
-                    String postid = rs.getString("postid");
-                    String title = rs.getString("title");
-                    String tag = rs.getString("tag");
+                    String postid = rs.getString("PostID");
+                    String title = rs.getNString("Title");
+                    String tag = rs.getNString("Tag");
                     //day:monnth:year  hour:minute
                     String approvedDate = Style.convertToDateFormat(rs.getInt("ApprovedDay"), rs.getInt("ApprovedMonth"),
                             rs.getInt("ApprovedYear"), rs.getInt("ApprovedHour"), rs.getInt("ApprovedMinute"));
@@ -136,7 +121,7 @@ public class ProfileDAO implements Serializable {
                     PostDTO dto = new PostDTO(postid, Style.convertTagToArrayList(tag), title, approvedDate, likes, comments);
                     boolean check = list.add(dto);
                     if (!check) {
-                        throw new SQLException("EY YO WTF");
+                        throw new SQLException("Adding a PostDTO failed!");
                     }
                 }
             }
